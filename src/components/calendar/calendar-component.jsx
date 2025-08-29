4244fd65f3a24aaa951367579da02ca466ae26fd
MarceloFOCMenezes <marcelo.menezes@sptech.school>
2025-08-29 19:16:10 -0300

    feat: adicionando pagina de calendario

---
+++ src/components/calendar/calendar-component.jsx
@@ -0,0 +1,33 @@
+import dayjs from 'dayjs';
+import updateLocale from "dayjs/plugin/updateLocale";
+import 'dayjs/locale/pt-br';
+import { DateCalendar, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { Container } from '@mui/material';
+import { styleCalendar } from './style';
+
+dayjs.extend(updateLocale);
+dayjs.updateLocale("pt-br", {
+    weekdaysMin: ["dom", "seg", "ter", "qua", "qui", "sex", "sáb"],
+    weekStart: 1, // deixe 1 (segunda) ou 0 (domingo) conforme seu layout
+});
+dayjs.locale("pt-br");
+
+export function CalendarComponent() {
+    return (
+        <Container sx={{width: '100%', p:0, height: '46vh', backgroundColor:'white'}}>
+            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pt-br">
+                <DateCalendar
+                    showDaysOutsideCurrentMonth
+                    minDate={dayjs().startOf('month')}
+                    // Gera "dom/seg/ter..." de acordo com a coluna real (sem deslocar)
+                    dayOfWeekFormatter={(date) =>
+                        dayjs(date).format('ddd').replace('.', '').toLowerCase()
+                    }
+                    
+                    sx={styleCalendar}
+                />
+            </LocalizationProvider>
+        </Container>
+    );
+}
--- src/pages/calendar-user/view/calendar-user.view.jsx
+++ src/pages/calendar-user/view/calendar-user.view.jsx
@@ -1,70 +1,62 @@
-import dayjs from 'dayjs';
-import updateLocale from "dayjs/plugin/updateLocale";
-import 'dayjs/locale/pt-br';
-import { DateCalendar, LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { Container } from '@mui/material';
-import { ThemeProvider } from '@mui/material/styles';
+import React, { useState } from 'react';
+import { Typography, Container, MenuItem, FormControl, InputLabel, Select, Button } from '@mui/material';
+import { CalendarComponent } from '../../../components/calendar/calendar-component';
 import theme from '../../../theme';
 
-
-// Locale + início da semana
-dayjs.extend(updateLocale);
-dayjs.updateLocale("pt-br", {
-  weekdaysMin: ["dom", "seg", "ter", "qua", "qui", "sex", "sáb"],
-  weekStart: 1, // deixe 1 (segunda) ou 0 (domingo) conforme seu layout
-});
-dayjs.locale("pt-br");
-
 export default function CalendarUserView() {
-  return (
-        <ThemeProvider theme={theme}>
+  const [horario, setHorario] = useState('');
 
-    <Container maxWidth="sm" sx={{ p: 2  }}>
-      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pt-br">
-        <DateCalendar
-          // Gera "dom/seg/ter..." de acordo com a coluna real (sem deslocar)
-          dayOfWeekFormatter={(date) =>
-            dayjs(date).format('ddd').replace('.', '').toLowerCase()
-          }
-          sx={(theme) => ({
-              width: '100%',
-              maxWidth: '100%',
-              mx: 'auto',
-              border: `1px solid ${theme.palette.primary.main}`, // usa a cor primária do tema
-              borderRadius: 4,
-    '& .MuiDayCalendar-weekDayLabel': {
-      width: 48,
-      lineHeight: '48px',
-      margin: 0,
-      textAlign: 'center',
-      fontWeight: 600,
-      textTransform: 'none',
-      boxSizing: 'border-box',
-      color: theme.palette.primary.main,
-    },
-    '& .MuiPickersDay-root': {
-        borderRadius: 0,
-        border: `1px solid #e0e0e0`,
-        margin: 0,
-        height: 48,
-        width: 48,
-        minWidth: 0,
-        minHeight: 0,
-        boxSizing: 'border-box',
-    },
-    '& .MuiPickersCalendarHeader-label': {
-        textTransform: 'capitalize',
-        color: theme.palette.primary.main,
-        fontWeight: 'bold',
-        fontSize: '1.25rem',
-    },
-    '& .MuiDayCalendar-weekContainer': { margin: 0 },
-    '& .MuiDayCalendar-header': { px: 0, mx: 0 },
-  })}
-        />
-      </LocalizationProvider>
-    </Container>
-    </ThemeProvider>
+  const handleChange = (event) => {
+    setHorario(event.target.value);
+  };
+
+  return (
+    <>
+      <Container sx={{ p: 2 }}>
+        <Typography variant='subtitle1' mb={2} fontWeight='bold' pl={2} color={theme.palette.primary.main}>
+          Escolha a data de entrega
+        </Typography>
+        <CalendarComponent />
+
+        <Typography variant='subtitle2' fontWeight='bold' pl={1} mt={2} mb={1} color={theme.palette.primary.main}>
+          Informe o horário de entrega
+        </Typography>
+
+        <FormControl sx={{ minWidth: '100%',  maxWidth: '100%'}} size="medium">
+          <InputLabel id="demo-select-small-label">Horário</InputLabel>
+          <Select
+            labelId="demo-select-small-label"
+            id="demo-select-small"
+            value={horario}
+            label="Horário"
+            onChange={handleChange}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={"09:00"}>9:00</MenuItem>
+            <MenuItem value={"10:00"}>10:00</MenuItem>
+            <MenuItem value={"11:00"}>11:00</MenuItem>
+            <MenuItem value={"12:00"}>12:00</MenuItem>
+            <MenuItem value={"13:00"}>13:00</MenuItem>
+            <MenuItem value={"14:00"}>14:00</MenuItem>
+            <MenuItem value={"15:00"}>15:00</MenuItem>
+            <MenuItem value={"16:00"}>16:00</MenuItem>
+            <MenuItem value={"17:00"}>17:00</MenuItem>
+            <MenuItem value={"18:00"}>18:00</MenuItem>
+            <MenuItem value={"19:00"}>19:00</MenuItem>
+            <MenuItem value={"20:00"}>20:00</MenuItem>
+          </Select>
+        </FormControl>
+        <Button
+          variant='contained'
+          color='primary'
+          type='submit'
+          sx={{ width: '100%', height: '48px', mt: 4 }}
+        >
+          Avançar
+        </Button>
+      </Container>
+    </>
   );
 }
