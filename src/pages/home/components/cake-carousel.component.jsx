aed4d4ebd724f1af38948e3371cac0ee96790f4d
izabelleFigueiredo <izabelle.figueiredo@sptech.school>
2025-08-30 15:40:57 -0300

    feat: add CakeCard and CakeCarousel components, and enhance HomeView layout

---
+++ src/pages/home/components/cake-carousel.component.jsx
@@ -0,0 +1,103 @@
+import Slider from 'react-slick';
+import { Box, IconButton } from '@mui/material';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { CakeCard } from './cake-card.component';
+import PropTypes from 'prop-types';
+
+// Componente para a seta "Anterior"
+function PrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <IconButton
+      onClick={onClick}
+      sx={{
+        position: 'absolute',
+        top: '55%', // Alinha verticalmente ao meio
+        left: '30%', // Posiciona à esquerda, fora da área dos cards
+        zIndex: 2,
+        transform: 'translateY(-50%)',
+        color: theme => theme.palette.primary.main,
+        border: theme => `2px solid ${theme.palette.primary.main}`,
+      }}
+    >
+      <ArrowBackIosNewIcon />
+    </IconButton>
+  );
+}
+
+// Componente para a seta "Próximo"
+function NextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <IconButton
+      onClick={onClick}
+      sx={{
+        position: 'absolute',
+        top: '55%', // Alinha verticalmente ao meio
+        right: '30%', // Posiciona à direita, fora da área dos cards
+        zIndex: 2,
+        transform: 'translateY(-50%)',
+        color: theme => theme.palette.primary.main,
+        border: theme => `2px solid ${theme.palette.primary.main}`
+      }}
+    >
+      <ArrowForwardIosIcon />
+    </IconButton>
+  );
+}
+
+export function CakeCarousel({ cakes }) {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true, // Para dar o efeito de centralizado
+    variableWidth: true, // Importante para larguras diferentes
+    className: "center", // Adiciona uma classe para estilização customizada
+    centerPadding: "60px", // cria espaço nas laterais
+    nextArrow: <NextArrow/>,
+    prevArrow: <PrevArrow/>,
+  };
+
+  return (
+    <Box
+      sx={{
+        width: '100%',
+        position: 'relative', //  ponto de referência para as setas
+        pb: 6, // padding embaixo para criar espaço para as setas
+        maxWidth: '400px',
+        height: '60dvh',
+        mx: 'auto',
+        py: 4,
+      }}
+    >
+      <Box sx={{ width: '100%', mx: 'auto', height: '100%', }}>
+        <Slider {...settings}>
+          {cakes.map(cake => (
+            <Box key={cake.id} sx={{ width: '280px !important' }}>
+              <CakeCard
+                image={cake.image}
+                title={cake.title}
+                description={cake.description}
+              />
+            </Box>
+          ))}
+        </Slider>
+      </Box>
+    </Box>
+  );
+}
+
+CakeCarousel.propTypes = {
+  cakes: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      image: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
--- src/pages/home/view/home.view.jsx
+++ src/pages/home/view/home.view.jsx
@@ -1,15 +1,149 @@
-import { Button, Checkbox } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { ROUTES_PATHS } from '../../../utils/enums/routes-url';
+import {
+  Box,
+  Button,
+  Container,
+  Stack,
+  Typography,
+  Card,
+  CardContent,
+  IconButton,
+} from '@mui/material';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { NavbarComponent } from '../../../components/navbar/navbar.component.jsx';
+import bgCakeImage from '../../../assets/cake-images/bg-cake.png';
+import { CakeCarousel } from '../components/cake-carousel.component';
+
+const mockCakes = [
+  {
+    id: 1,
+    title: 'Bolo Floral',
+    description: 'Bolo de 2 andares com flores de açúcar',
+    image: bgCakeImage,
+  },
+  {
+    id: 2,
+    title: 'Bolo Azul',
+    description: 'Bolo decorado com buttercream azul e detalhes dourados',
+    image: bgCakeImage,
+  },
+  {
+    id: 3,
+    title: 'Bolo de Frutas',
+    description: 'Bolo com frutas vermelhas frescas e creme',
+    image: bgCakeImage,
+  },
+];
+
 export function HomeView() {
   const navigate = useNavigate();
 
   return (
     <>
-      <h1>Home View</h1>
-      <Button variant='contained' color='primary' onClick={() => navigate(ROUTES_PATHS.REGISTER_USER)}>
-        Ação Principal
-      </Button>
+      <NavbarComponent></NavbarComponent>
+
+      <Container // banner
+        sx={{
+          width: '100%',
+          height: '55vh',
+          p: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: 4,
+
+          backgroundImage: `url(${bgCakeImage})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        <Stack // banner
+          sx={{
+            textAlign: 'left',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            height: '90%',
+          }}
+        >
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 4,
+              alignItems: 'center',
+            }}
+          >
+            <Typography variant='h4' color='tertiary' fontWeight={600}>
+              Seu pedido, do seu jeito!
+            </Typography>
+            <Typography variant='h8' color='tertiary'>
+              Escolha sabores, tamanhos e detalhes para deixar tudo do seu
+              jeitinho.
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              width: '100%',
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <Button
+              variant='contained'
+              color='primary'
+              onClick={() => navigate(ROUTES_PATHS.NUMERO_CONVIDADOS)}
+              // Alteração 2: Ajustes de largura e margem no próprio botão
+              sx={{
+                width: { xs: '80%', sm: '60%', md: '50%' }, // O botão será mais largo
+                py: 1.5, // Aumenta a altura do botão (padding vertical)
+              }}
+            >
+              Personalizar bolo
+            </Button>
+          </Box>
+        </Stack>
+      </Container>
+
+      <Container
+        sx={{
+          p: '3, 3, 0',
+          marginTop: 4,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          textAlign: 'center',
+        }}
+      >
+
+        <Stack // referencias
+          sx={{
+            gap: 2,
+          }}
+        >
+          <Typography variant='body1'>Referências</Typography>
+          <Typography variant='h5' fontWeight={600}>
+            Se inspire para seu pedido
+          </Typography>
+        </Stack>
+      </Container>
+      <CakeCarousel cakes={mockCakes} />
+
+      {/* <Container sx={{ p: 3 }}>
+        <Button
+          variant='contained'
+          color='primary'
+          onClick={() => navigate(ROUTES_PATHS.REGISTER_USER)}
+        >
+          Cadastro
+        </Button>
+      </Container> */}
     </>
   );
 }
