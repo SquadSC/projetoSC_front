31eb4e6b7412270195f7dc114dcef428d40e7cce
Eduardo Alves Azevedo <eduardo.azevedo@sptech.school>
2025-09-18 19:40:48 -0300

    change: adding base template for order customization screen

--- src/pages/order-summary-cake/view/order-summary-cake.view.jsx
+++ src/pages/order-summary-cake/view/order-summary-cake.view.jsx
@@ -1,9 +1,38 @@
-import { Box } from '@mui/material';
+import { Box, Container } from '@mui/material';
+import { PageHeader } from '../../../components/header-jornada/header-jornada.component';
+import { StepperComponent } from '../../../components/stepper/stepper-component';
+import { ModelComponent } from '../components/model/model.component';
 
-export function OrderSummaryCakeView() {
+export function OrderSummaryCakeView({
+  stepConfig: { nextStep, activeStep, maxStepReached, setActiveStep },
+}) {
+  const steps = ['Etapa 1', 'Etapa 2', 'Etapa 3', 'Etapa 4'];
+
+  const getStepContent = step => {
+    switch (step) {
+      case 0:
+        return <ModelComponent nextStep={nextStep} />;
+      case 1:
+        return <ModelComponent nextStep={nextStep} />;
+      case 2:
+        return <ModelComponent nextStep={nextStep} />;
+      case 3:
+        return <ModelComponent nextStep={nextStep} />;
+    }
+  };
+
   return (
-    <Box
-      sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}
-    ></Box>
+    <>
+      <Container sx={{ p: 3 }}>
+        <PageHeader titulo='Pedido Personalizado' showBackButton={true} />
+        <StepperComponent
+          steps={steps}
+          activeStep={activeStep}
+          maxStepReached={maxStepReached}
+          onStepChange={setActiveStep}
+        />
+      </Container>
+      <Box> {getStepContent(activeStep)} </Box>
+    </>
   );
 }
